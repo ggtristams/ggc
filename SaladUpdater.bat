070457a144436d3672aaebecd86399cc96fc1686
ggtristams <136493981+ggtristams@users.noreply.github.com>
2024-09-04 02:30:48 +0800

    Update SaladUpdater.bat

--- SaladUpdater.bat
+++ SaladUpdater.bat
@@ -15,30 +15,41 @@ set "tempDir=%temp%\Wub"
 if exist "%tempFile%" del "%tempFile%"
 if exist "%tempDir%" rmdir /s /q "%tempDir%"
 
-:: Check if Salad is already installed
+:: goto :checkFileExistence
+
+:checkFileExistence
+echo Checking if Salad is already installed...
 if exist "%versionFile%" (
-    echo Checking if Salad version is the latest...
-    type "%versionFile%" > "%tempFile%"
-    set /p installedVersion=<"%tempFile%"
-    echo - Installed Version: %installedVersion%
-    if /i "%installedVersion%"=="%requiredVersion%" (
-        echo Salad version is the latest.
-        goto :abort
-    ) else (
-        echo Salad version is outdated or different.
-    )
+    echo - Salad is already installed.
+    goto :extractVersion
 ) else (
     echo %versionFile% not found.
+    goto :searchAndRun
 )
 
-:: Search for the target file
+:extractVersion
+echo Extracting installed version...
+type "%versionFile%" > "%tempFile%"
+goto :compareVersion
+
+:compareVersion
+echo Checking if installed version is the latest...
+set /p installedVersion=<"%tempFile%"
+echo - Installed Version: %installedVersion%
+
+if /i "%installedVersion%"=="%requiredVersion%" (
+    goto :abort
+) else (
+    echo Salad version is outdated or different.
+)
+
+:searchAndRun
 echo Searching for %targetFile% in %searchDir% and all subfolders...
 for /f "delims=" %%i in ('dir /s /b "%searchDir%\%targetFile%" 2^>nul') do (
     set "foundFile=%%i"
     goto :runFile
 )
 
-:: Check temporary directory for the target file
 echo %targetFile% not found in %searchDir%. Checking temporary directory...
 if exist "%tempDir%\%targetFile%" (
     echo %targetFile% already exists in %tempDir%.
@@ -80,6 +91,7 @@ echo Checking if wuauserv is running...
 sc query wuauserv | findstr /i "RUNNING" >nul
 if %errorlevel% equ 0 (
     echo wuauserv is already running.
+    echo.
     choice /c YN /n /t 10 /d N /m "Would you like to restart the ggLeap client? (Y/N): "
     if %errorlevel% equ 1 (
         echo Stopping clientinterface.exe if it's running...
@@ -91,6 +103,7 @@ if %errorlevel% equ 0 (
     )
 ) else (
     echo wuauserv failed to start, reboot may be required.
+    echo.
     choice /c YN /n /t 10 /d N /m "Would you like to reboot the system now? (Y/N): "
     if %errorlevel% equ 1 (
         echo Initiating system reboot...
@@ -110,7 +123,7 @@ if exist "%tempFile%" del "%tempFile%"
 if exist "%tempDir%" rmdir /s /q "%tempDir%"
 
 :end
-echo Cleanup completed.
+echo - Cleanup completed.
 endlocal
 timeout /t 2
 exit /b
